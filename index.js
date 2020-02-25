052f06b12791f1789af1b46266824203562c9dd6
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2020-02-25 10:37:01 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+/* globals RunKit */
+
+customElements.define('run-kit', class extends HTMLElement {
+  connectedCallback() {
+    const root = this.attachShadow({
+      mode: 'open',
+    })
+    root.appendChild(document.createElement('slot'))
+
+    if (!window.RunKit) {
+      const runkitScript = document.createElement('script')
+      runkitScript.setAttribute('src', 'https://embed.runkit.com')
+      runkitScript.onload = this.__init.bind(this)
+      document.head.appendChild(runkitScript)
+    } else {
+      this.__init()
+    }
+  }
+
+  __init() {
+    const code = this.querySelector('pre')
+
+    this.runKit = RunKit.createNotebook({
+      element: this,
+      gutterStyle: 'inside',
+      source: code.textContent.trim(),
+    })
+
+    this.runKit.onLoad = () => code.remove()
+  }
+})
